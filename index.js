41274507e813e3c0e17e07519f1e7ada4d0bd707
jensvog <62656589+jensvog@users.noreply.github.com>
2022-02-13 15:32:29 +0100

    Reload node script

---
+++ index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+try {
+    const host = core.getInput('host');
+    const username = core.getInput('username');
+    const apiToken = core.getInput('api-token');
+    const domainName = core.getInput('app-name');
+
+    const url = `https://${host}/api/v0/user/${username}/webapps/${domainName}/reload/`
+
+    console.log('Trying to reload webapp...')
+    
+    axios.post(url, null, {
+        headers: {"Authorization" : `Token ${apiToken}`}
+    })
+    .then(function (response) {
+        console.log('Reloaded webapp successfully.')
+    })
+    .catch(function (error) {
+        core.setFailed(error.message);
+    });
+} catch (error) {
+    core.setFailed(error.message);
+}
